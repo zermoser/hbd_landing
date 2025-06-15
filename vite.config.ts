73a8c38dc95f9af33dee9dd12fb2458e3df9c1c6
zermoser <kittipoj.na@gmail.com>
2025-06-15 23:55:02 +0700

    chore: update vite config

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/hbd-landing/',
+  base: '/hbd_landing/',
   server: {
     open: true,
     port: 3006
